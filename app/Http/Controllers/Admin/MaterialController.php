823141a5bb9230b90583601c60b4e8f3e4e75b61
Gabriel <gs6850061@gmail.com>
2022-08-16 21:36:54 -0300

    Meu primeiro commit

---
+++ app/Http/Controllers/Admin/MaterialController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MaterialController extends Controller
+{
+    public function index(Request $request)
+    {
+        return view('material.index');
+    }
+    public function create(Request $request)
+    {
+        
+    }
+    public function show(Request $request)
+    {
+        
+    }
+    public function edit(Request $request)
+    {
+        
+    }
+    public function update(Request $request)
+    {
+        
+    }
+    public function destroy(Request $request)
+    {
+        
+    }
+    public function rules(Request $request)
+    {
+        
+    }
+}
---
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -0,0 +1,42 @@
+<div class="sidebar">
+    <div class="sidebar-wrapper">
+        <div class="logo">
+            <a href="#" class="simple-text logo-mini">{{ _('WD') }}</a>
+            <a href="#" class="simple-text logo-normal">{{ _('White Dashboard') }}</a>
+        </div>
+        <ul class="nav">
+            <li @if ($pageSlug == 'dashboard') class="active " @endif>
+                <a href="{{ route('home') }}">
+                    <i class="tim-icons icon-chart-pie-36"></i>
+                    <p>{{ _('Dashboard') }}</p>
+                </a>
+            </li>
+            <li @if ($pageSlug == 'dashboard') class="active " @endif>
+                <a href="{{ route('material.index') }}">
+                    <i class="tim-icons icon-chart-pie-36"></i>
+                    <p>{{ _('Material') }}</p>
+                </a>
+            </li>
+            <li @if ($pageSlug == 'dashboard') class="active " @endif>
+                <a href="{{ route('home') }}">
+                    <i class="tim-icons icon-chart-pie-36"></i>
+                    <p>{{ _('Cliente') }}</p>
+                </a>
+            </li>
+            <li @if ($pageSlug == 'dashboard') class="active " @endif>
+                <a href="{{ route('home') }}">
+                    <i class="tim-icons icon-chart-pie-36"></i>
+                    <p>{{ _('Reserva') }}</p>
+                </a>
+            </li>
+            <li @if ($pageSlug == 'dashboard') class="active " @endif>
+                <a href="{{ route('home') }}">
+                    <i class="tim-icons icon-chart-pie-36"></i>
+                    <p>{{ _('Usuarios') }}</p>
+                </a>
+            </li>        
+
+                
+        </ul>
+    </div>
+</div>
