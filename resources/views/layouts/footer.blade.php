143226c926b98b978ee2761f0a70eb1f7f1ee82a
Mauricio Macedo <macedo.mauriciogen@gmail.com>
2022-09-10 15:09:51 +0000

    git funcionando :)

--- resources/views/layouts/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<footer class="footer">
-    <div class="container-fluid">
-        <div class="copyright">
-            &copy; {{ now()->year }} {{ _('Desenvolvido ') }}  {{ _('por') }}
-            {{ _('Mauricio Macedo') }}.
-        </div>
-    </div>
-</footer>
